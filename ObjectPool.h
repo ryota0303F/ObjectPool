4dd5c05c660347d87b147e72fc4a1167f55db407
Mizuki <65876541+ryota0303F@users.noreply.github.com>
2021-05-08 15:56:32 +0900

    Add files via upload
    vectorをポインタメンバからメンバ変数に変更

--- ObjectPool.h
+++ ObjectPool.h
@@ -21,15 +21,14 @@ public:
 	void Release();
 
 private:
-	std::vector<T*>* m_objectlist;
-	std::vector<bool>* m_objectchlist;
+	std::vector<T*> m_objectlist;
+	std::vector<bool> m_objectchlist;
 };
 
 template<class T>
 inline ObjectPool<T>::ObjectPool()
 {
-	m_objectlist = new std::vector<T*>();
-	m_objectchlist = new std::vector<bool>();
+
 }
 
 template<class T>
@@ -43,20 +42,20 @@ inline void ObjectPool<T>::SetPlus(const unsigned int addsize)
 {
 	for (unsigned int i = 0; i < addsize; i++)
 	{
-		m_objectlist->push_back(new T);
-		m_objectchlist->push_back(false);
+		m_objectlist.push_back(new T);
+		m_objectchlist.push_back(false);
 	}
 }
 
 template<class T>
 inline T* ObjectPool<T>::GetObject()
 {
-	for (unsigned int i = 0; i < m_objectchlist->size(); i++)
+	for (unsigned int i = 0; i < m_objectchlist.size(); i++)
 	{
-		if (m_objectchlist->at(i) == false)
+		if (m_objectchlist.at(i) == false)
 		{
-			m_objectchlist->at(i) = true;
-			return m_objectlist->at(i);
+			m_objectchlist.at(i) = true;
+			return m_objectlist.at(i);
 		}
 	}
 	//m_objectchlist->at(index) = true;
@@ -68,12 +67,12 @@ template<class T>
 inline void ObjectPool<T>::ReturnObject(T** object)
 {
 	int index = 0;
-	for (unsigned int i = 0; i < m_objectlist->size(); i++)
+	for (unsigned int i = 0; i < m_objectlist.size(); i++)
 	{
-		if (*object == m_objectlist->at(i))
+		if (*object == m_objectlist.at(i))
 		{
 			*object = nullptr;
-			m_objectchlist->at(i) = false;
+			m_objectchlist.at(i) = false;
 			return;
 		}
 	}
@@ -83,17 +82,17 @@ inline void ObjectPool<T>::ReturnObject(T** object)
 template<class T>
 inline unsigned int ObjectPool<T>::GetSize()
 {
-	return m_objectlist->size();
+	return m_objectlist.size();
 }
 
 template<class T>
 inline bool ObjectPool<T>::Getactive(T* object)
 {
-	for (unsigned int i = 0; i < m_objectlist->size(); i++)
+	for (unsigned int i = 0; i < m_objectlist.size(); i++)
 	{
-		if (object == m_objectlist->at(i))
+		if (object == m_objectlist.at(i))
 		{
-			return m_objectchlist->at(i);
+			return m_objectchlist.at(i);
 		}
 	}
 	return false;
@@ -102,11 +101,11 @@ inline bool ObjectPool<T>::Getactive(T* object)
 template<class T>
 inline void ObjectPool<T>::Release()
 {
-	for (unsigned int i = 0; i < m_objectlist->size(); i++)
+	for (unsigned int i = 0; i < m_objectlist.size(); i++)
 	{
-		delete m_objectlist->at(i);
+		delete m_objectlist.at(i);
 	}
-	m_objectchlist->clear();
-	delete m_objectchlist;
-	delete m_objectlist;
+	m_objectchlist.clear();
+	m_objectlist.clear();
+
 }
